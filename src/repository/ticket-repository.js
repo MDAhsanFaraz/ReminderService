c0c70118796e47cb918c7bccc6f645b314974b46
Faraz <faraz.com>
2023-08-21 01:21:03 +0530

    Added notification cron

---
+++ src/repository/ticket-repository.js
@@ -0,0 +1,57 @@
+const { NotificationTicket } = require('../models/index');
+const { Op } = require("sequelize");
+
+class TicketRepository {
+    async getAll(){
+        try {
+            const tickets=await NotificationTicket.findAll();
+            return tickets;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async create(data){
+        try {
+            const ticket=await NotificationTicket.create(data);
+            return ticket;
+        } catch (error) {
+            
+        }
+    }
+    async get(fliter)
+    {
+        try {
+            const tickets =await NotificationTicket.findAll({
+                where:{
+                    status:fliter.status,
+                    notificationTime:{
+                        [Op.lte]:new Date()
+                    }
+                }
+            });
+            return tickets;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async update(ticketId,data)
+    {
+        try {
+            const ticket=await NotificationTicket.findByPk(ticketId);
+            if(data.status)
+            {
+                ticket.status=data.status;
+            }
+            await ticket.save();
+            return ticket;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+
+
+}
+
+module.exports=TicketRepository;
